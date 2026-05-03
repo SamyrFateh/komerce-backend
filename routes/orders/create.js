72f71036b02261020dc6742e3a2f2f667b149ed6
SamyrFateh <fatehsamyr@gmail.com>
2026-05-03 20:16:57 +0200

    feat ajout des variantes vagues 3 et dashboard variantes 1

--- routes/orders/create.js
+++ routes/orders/create.js
@@ -8,7 +8,7 @@
 
 const express = require('express');
 const router  = express.Router();
-const { v4: uuidv4 } = require('uuid');
+const { randomUUID: uuidv4 } = require('crypto');
 const db      = require('../../db');
 const { authenticate }                   = require('../../middleware/auth');
 const { authenticateOrCreateGuest }      = require('../../middleware/auth-guest');
