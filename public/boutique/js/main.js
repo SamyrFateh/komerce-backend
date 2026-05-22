c0581094499e34de5eb0192bab512c9513478eae
SamyrFateh <fatehsamyr@gmail.com>
2026-05-23 01:12:33 +0200

    restore(boutique): disable mobile premium layer

--- public/boutique/js/main.js
+++ public/boutique/js/main.js
@@ -10,7 +10,6 @@ import { setupModalContractClasses } from './b-modal-desktop-enhancers.js';
 import { setupApprocheCHybridPdp } from './b-modal-approche-c-hybrid.js';
 import { setupPdpCurationSuggestions } from './b-pdp-curation-suggestions.js';
 import { setupHomePremiumV1 } from './b-home-premium-v1.js';
-import { setupMobilePremiumV1 } from './b-mobile-premium-v1.js';
 
 function setupBoutiqueRuntime() {
   setupModalContractClasses();
@@ -18,7 +17,6 @@ function setupBoutiqueRuntime() {
   setupApprocheCHybridPdp();
   setupPdpCurationSuggestions();
   setupHomePremiumV1();
-  setupMobilePremiumV1();
   setupProductOpenContract();
   setupCartProductOpenStyle();
 }
@@ -44,7 +42,6 @@ if (typeof window !== 'undefined') {
         setupApprocheCHybridPdp();
         setupPdpCurationSuggestions();
         setupHomePremiumV1();
-        setupMobilePremiumV1();
       }
     }, 150);
   }, { passive: true });
