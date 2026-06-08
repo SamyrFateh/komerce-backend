382cb25cd34b647f18a422bc0f574651290f080c
SamyrFateh <fatehsamyr@gmail.com>
2026-06-08 13:29:25 +0200

    Feature ajout paypal + dernières corrections 5

--- public/boutique/scripts/deploy-css.js
+++ public/boutique/scripts/deploy-css.js
@@ -46,7 +46,7 @@ const BUNDLES = [
   {
     out: 'components.css',
     files: ['categories', 'products', 'modal-shell', 'modal-media', 'modal-product', 'modal-product-lot4-hybrid',
-            'cart', 'interactions', 'hero-cart-proxy', 'group-cart-flow', 'shared-followup', 'identity'],
+            'cart', 'interactions', 'hero-cart-proxy', 'group-cart-flow', 'shared-followup', 'identity', 'paypal'],
   },
   {
     out: 'desktop.css',
