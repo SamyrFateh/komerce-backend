abfb44948365b878dd33fcd5991b23d00229beca
SamyrFateh <fatehsamyr@gmail.com>
2026-06-30 00:01:19 +0200

    test(finance): cover finance metrics barrel

---
+++ tests/unit/finance-metrics-index.test.js
@@ -0,0 +1,18 @@
+'use strict';
+
+describe('finance-metrics/index', () => {
+  it('re-exporte les quatre fonctions publiques du dashboard finance', () => {
+    const metrics = require('../../services/finance-metrics');
+
+    expect(typeof metrics.getFinanceSummary).toBe('function');
+    expect(typeof metrics.getAnnulationsParcels).toBe('function');
+    expect(typeof metrics.getPaymentsDetail).toBe('function');
+    expect(typeof metrics.getSalesAnalysis).toBe('function');
+    expect(Object.keys(metrics).sort()).toEqual([
+      'getAnnulationsParcels',
+      'getFinanceSummary',
+      'getPaymentsDetail',
+      'getSalesAnalysis',
+    ].sort());
+  });
+});
