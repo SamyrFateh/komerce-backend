0164bebea8edb31c4530d9abab38dbffac7ba33e
SamyrFateh <fatehsamyr@gmail.com>
2026-08-01 15:05:08 +0200

    test: load deterministic countdown setup

--- public/boutique/jest.config.js
+++ public/boutique/jest.config.js
@@ -8,6 +8,7 @@ module.exports = {
   },
   transformIgnorePatterns: ['/node_modules/'],
   setupFiles: ['./tests/unit/setup.js'],
+  setupFilesAfterEnv: ['./tests/unit/setup-after-env.js'],
   collectCoverageFrom: [
     'js/**/*.js',
     '!js/dist/**',
