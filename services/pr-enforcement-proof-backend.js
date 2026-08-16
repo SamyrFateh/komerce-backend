218a578f55956ffdcc31c240d1d42d8a1245106c
SamyrFateh <fatehsamyr@gmail.com>
2026-08-16 17:26:43 +0200

    Revert "proof backend gate rejects console.log"

--- services/pr-enforcement-proof-backend.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-// Controlled negative proof: this console.log must be rejected by code-quality-gate --strict.
-console.log('pr-enforcement backend negative proof');
-
-module.exports = {};
