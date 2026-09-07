c30fc3850973bbdecaf13cb183f7da81251714a6
SamyrFateh <fatehsamyr@gmail.com>
2026-09-07 12:08:29 +0200

    fix(catalog): restore UUID route validation

--- routes/products.js
+++ routes/products.js
@@ -43,7 +43,7 @@ const {
 const { applyActiveMarketPricesToCatalogRows } = require('../services/market-local-price-resolution-service');
 const log = require('../utils/logger').child({ module: 'products' });
 
-const UUID_RE = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
+const UUID_RE = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
 
 function requireUUID(req, res, next) {
   if (!UUID_RE.test(req.params.id)) {
