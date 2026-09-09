8dae6dad42dc8f8bccaa9bc2852f556fed4ede41
SamyrFateh <fatehsamyr@gmail.com>
2026-09-09 22:37:54 +0200

    chore(market-delegation): rattacher les services à la slice

--- services/capability-registry.js
+++ services/capability-registry.js
@@ -1,7 +1,7 @@
 /**
  * @komerce-arch
  * @role          market-delegation-capability-registry
- * @domain        market
+ * @domain        market-delegation
  * @layer         service
  * @criticality   high
  * @inputs        capability_registry
